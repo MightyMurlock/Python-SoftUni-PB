5d81a3d18ec2ff49b73e680cfda9cec2885b77d2
Konstantin <63278826+MightyMurlock@users.noreply.github.com>
2024-10-19 22:42:14 +0300

    Updated

---
+++ Conditional Statements Advanced - Lab/P12.py
@@ -0,0 +1,44 @@
+city = str(input())
+sales = float(input())
+
+match city:
+    case "Sofia":
+        if sales >= 0 and sales <= 500:
+            commission = 5 / 100
+        elif sales > 500 and sales <= 1000:
+            commission = 7 / 100
+        elif sales > 1000 and sales <= 100000:
+            commission = 8 / 100
+        elif sales > 10000:
+            commission = 12 / 100
+        else:
+            commission = 0
+    case "Varna":
+        if sales >= 0 and sales <= 500:
+            commission = 4.5 / 100
+        elif sales > 500 and sales <= 1000:
+            commission = 7.5 / 100
+        elif sales > 1000 and sales <= 100000:
+            commission = 10 / 100
+        elif sales > 10000:
+            commission = 13 / 100
+        else:
+            commission = 0
+    case "Plovdiv":
+        if sales >= 0 and sales <= 500:
+            commission = 5.5 / 100
+        elif sales > 500 and sales <= 1000:
+            commission = 8 / 100
+        elif sales > 1000 and sales <= 100000:
+            commission = 12 / 100
+        elif sales > 10000:
+            commission = 14.5 / 100
+        else:
+            commission = 0
+    case _:
+        commission = 0
+
+if commission != 0:
+    print(f"{commission * sales:.2f}")
+else:
+    print("error")
